1847472d50091aa09da4ce85c135a589f9aca8e0
KakolJakub <kakoljakub@wp.pl>
2023-04-18 19:18:48 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
