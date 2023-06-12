c713dfdddba1da09fc993e5a9c27a7af69aa0943
KakolJakub <kakoljakub@wp.pl>
2023-06-12 21:08:38 +0200

    End of section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDTO>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(x => x.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(x => x.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDTO>();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -28,4 +28,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration.");
+}
+
 app.Run();
